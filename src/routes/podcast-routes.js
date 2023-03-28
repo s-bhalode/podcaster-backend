fe931687b4f6d4fed76bc615502bda2afbcc00c4
Hardik panwar <hardikpanwar63@gmail.com>
2023-03-28 15:23:36 +0530

    Req params chnages

--- src/routes/podcast-routes.js
+++ src/routes/podcast-routes.js
@@ -3,28 +3,31 @@ const router = new express.Router();
 const podcastController = require('../controller/podcast-controller');
 
 //create podcast
-router.post('/api/create-podcast', podcastController.createPodcast);
+router.post('/api/create-podcast/:userId', podcastController.createPodcast);
+
+//create Episode
+router.post('/api/podcasts/create-episode/:podcastId/:userId',podcastController.createEpisodes)
 
 //Get All podcasts Data
 router.get('/api/podcasts', podcastController.getAllPodcast);
 
 //Get All podcast details by Category (filtered)
-router.get('/api/podcasts/:id/:category',podcastController.getPodcastbyCategory);
+router.get('/api/podcasts/:category',podcastController.getPodcastbyCategory);
 
 // get podcast by Id
-router.get('/api/podcast/:id', podcastController.getPodcastById);
+router.get('/api/podcast/:podcastId', podcastController.getPodcastById);
 
 //Update the particular podcast by id
-router.put('/api/podcast/update-podcast/:id',podcastController.updatePodcastById);
+router.put('/api/podcast/update-podcast/:userId/:podcastId',podcastController.updatePodcastById);
 
 //pushing comments into the podcast
-router.post('/api/podcast/comment/:id',podcastController.pushCommentsIntoPodcastbyId);
+router.post('/api/podcast/comment/:userId/:podcastId',podcastController.pushCommentsIntoPodcastbyId);
 
 //pushing likes of the podcast by Id
-router.post('/api/podcast/like/:id',podcastController.pushLikesIntoPodcastbyId);
+router.post('/api/podcast/like/:userId/:podcastId',podcastController.pushLikesIntoPodcastbyId);
 
 // getting the particular user podcast data 
-router.get('/api/user/:id/podcast',podcastController.getUserPodcastData)
+router.get('/api/user/:userId/podcast',podcastController.getUserPodcastData)
 
 
 
