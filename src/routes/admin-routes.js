38665cb26d2dce5f21db1e798987c2264ebbc609
Simran Bhalode <simran.bhalode@gmail.com>
2023-08-27 22:57:46 +0530

    user-count, post-count, podcast-count

--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = new express.Router();
 const adminController = require('../controller/admin-controller')
 
-router.get('/api/admin/dashboard/get-all-user-details',adminController.getalluserData)
+router.get('/api/admin/dashboard/get-all-user-details',adminController.getalluserData);
+
+router.get('/api/admin/dashboard/get-post-count',adminController.getPostCount);
+
+router.get('/api/admin/dashboard/get-podcast-count',adminController.getPodcastCount);
+
+router.get('/api/admin/dashboard/get-user-count',adminController.getUserCount);
 
 
 
